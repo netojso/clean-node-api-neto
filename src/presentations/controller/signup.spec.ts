0c07df1e5ab3cb25734a013cf0a36bc74bd0267c
Octacilio Serafim <neto.devx@gmail.com>
2021-03-14 11:53:43 -0300

    feat: ensure SignUpController returns a error if name is undefined

--- src/presentations/controller/signup.spec.ts
+++ src/presentations/controller/signup.spec.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from "./signup";
+import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('should return 400 if field name is undefined', () => {
@@ -14,5 +14,6 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
