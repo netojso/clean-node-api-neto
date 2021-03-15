53d657c1fafe234a2053876faf945e71ae40a750
Octacilio Serafim <neto.devx@gmail.com>
2021-03-15 14:30:41 -0300

    refactor: move protocols and erros to single file

--- src/presentations/controller/signup.ts
+++ src/presentations/controller/signup.ts
@@ -1,10 +1,6 @@
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { MissingParamsError } from '../errors/missing-params-error'
-import { ServerError } from '../errors/server-error'
+import { InvalidParamError, MissingParamsError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { Controller, EmailValidator, HttpResponse, HttpRequest } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
