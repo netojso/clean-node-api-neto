6e3b0ccac0e505125ea103a86e87c50b8a8c2751
Octacilio Serafim <neto.devx@gmail.com>
2021-03-17 14:46:12 -0300

    feat: ensure AccountMongoRepository returns account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,10 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
-export const MongoHelper = {
-  client: null as MongoClient,
+interface MongoHelperTypes {
+  client: MongoClient | null
+  connect: (uri: string) => Promise<void>
+  disconnect: () => Promise<void>
+  getCollection: (name: string) => Collection
+}
+
+export const MongoHelper: MongoHelperTypes = {
+  client: null,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri, {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
@@ -12,5 +19,9 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
