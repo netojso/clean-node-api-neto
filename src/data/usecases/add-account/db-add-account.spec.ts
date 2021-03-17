672a859dc5beadbf188d79901787fbf2175f1e91
Octacilio Serafim <neto.devx@gmail.com>
2021-03-17 08:50:36 -0300

    test: ensure DbAddAccount return a new account

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -105,4 +105,23 @@ describe('DbAddAccount UseCase', () => {
     const promise = sut.add(account)
     expect(promise).rejects.toThrow()
   })
+
+  test('should return an account on succes', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
