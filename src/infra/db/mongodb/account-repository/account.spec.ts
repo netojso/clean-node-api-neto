6d9ffa8949a911b8156132af3ddb2dde68b6dba5
Octacilio Serafim <neto.devx@gmail.com>
2021-03-17 14:53:33 -0300

    refactor: clear db between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -13,6 +13,11 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('should return an account on success', async () => {
     const sut = makeSut()
 
