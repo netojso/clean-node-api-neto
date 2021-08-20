d871ed1a8a139dace89c05d5b436aa7b63d1809e
netojso <neto.devx@gmail.com>
2021-08-20 13:07:02 -0300

    test: ensure returns from RequiredFieldValidation

---
+++ src/presentations/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,19 @@
+import { MissingParamsError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+describe('RequiredField Validation', () => {
+  test('should return a MissingParamsError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamsError('field'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
+})
