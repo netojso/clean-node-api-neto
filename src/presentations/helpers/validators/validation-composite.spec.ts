34d37225ce58bde26625bd97de55868ef53662ac
netojso <neto.devx@gmail.com>
2021-08-20 13:36:26 -0300

    test: add tests to ValidationComposite

--- src/presentations/helpers/validators/validation-composite.spec.ts
+++ src/presentations/helpers/validators/validation-composite.spec.ts
@@ -36,4 +36,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamsError('field'))
   })
+
+  test('should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
