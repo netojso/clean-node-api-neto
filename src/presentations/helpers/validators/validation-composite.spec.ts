5512074e673240f64f4b5be638bf5495f6b3f62c
netojso <neto.devx@gmail.com>
2021-08-20 13:28:32 -0300

    test: add tests to ValidationComposite

---
+++ src/presentations/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,39 @@
+import { MissingParamsError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('Validation Composite', () => {
+  test('should return an error if any validation fails', () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamsError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamsError('field'))
+  })
+})
