f462661e5dafa7e0af015762f85fe271786a8a30
netojso <neto.devx@gmail.com>
2021-09-30 17:21:29 -0300

    test: ensure AuthMiddleware returns 200 if LoadAccountByToken returns an account

--- src/presentations/middlewares/auth-middleware.spec.ts
+++ src/presentations/middlewares/auth-middleware.spec.ts
@@ -1,6 +1,6 @@
 import { AuthMiddleware } from './auth-middleware'
 import { AccessDeniedError } from '../errors'
-import { forbidden } from '../helpers/http/http-helper'
+import { forbidden, ok } from '../helpers/http/http-helper'
 import { AccountModel } from '../../domain/models/account'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
@@ -59,5 +59,15 @@ describe('Auth Middleware', () => {
       }
     })
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('should return 200 if LoadAccountByToken returns an account', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(httpResponse).toEqual(ok({ accountId: 'valid_id' }))
   })
 })
