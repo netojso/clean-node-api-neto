23475928e3219d08576f23cc0c67c626833eebb6
netojso <neto.devx@gmail.com>
2022-03-31 13:39:25 -0300

    refactor: change AddSurvey to insert a date also

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -7,7 +7,8 @@ const makeFakeSurvey = (): AddSurveyModel => ({
   answers: [{
     image: 'any_image',
     answer: 'any_answer'
-  }]
+  }],
+  date: new Date()
 })
 
 const makeAddSurveyRepository = (): AddSurveyRepository => {
